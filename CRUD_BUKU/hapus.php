cd5df55f626796de8751be3ccce3583fbdd84784
Arief862 <146642863+Arief862@users.noreply.github.com>
2025-04-23 14:19:04 +0700

    Delete CRUD_BUKU directory

--- CRUD_BUKU/hapus.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'koneksi.php';
-$id = $_GET['id'];
-mysqli_query($koneksi,"DELETE FROM buku WHERE id=$id");
-header("Location: index.php");
-?>
--- CRUD_BUKU/koneksi.php
+++
@@ -1,5 +0,0 @@
-<?php
-$koneksi = mysqli_connect(hostname: "localhost", username:"root", password:"", database:"db_buku");
-if  (!$koneksi){
-    die("koneksi gagal: " . mysqli_connect_error());
-}
